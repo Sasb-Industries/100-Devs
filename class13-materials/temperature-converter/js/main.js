ea46110b19e11717094d39880bcfeb8ab1ae7e15
Sasb-Industries <sashkajohnson@gmail.com>
2022-05-30 17:36:24 -0400

    Temp Converter

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,9 @@
-//Write your pseduo code first! 
+document.querySelector('#btn').addEventListener('click', convert)
+
+function convert () {
+    const temp = document.querySelector('#input').value
+    
+    output = temp * 9/5 + 32;
+
+    document.querySelector('#output').innerText = output
+}
